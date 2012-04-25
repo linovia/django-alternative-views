33ac49f6662f5b82fa61c4f0f97b3440a24969c9
Xavier Ordoquy <xordoquy@linovia.com>
2012-04-25 21:39:26 +0200

    Cleaned a bit the imports.

--- local_tests/tests/object_urls.py
+++ local_tests/tests/object_urls.py
@@ -1,8 +1,6 @@
 from __future__ import absolute_import
 
-from django.conf.urls import patterns, url
-from django.views.decorators.cache import cache_page
-# from django.views.generic import TemplateView
+from django.conf.urls import patterns
 
 from alternative_views.base import View
 from alternative_views.mixins.object import ObjectMixin
