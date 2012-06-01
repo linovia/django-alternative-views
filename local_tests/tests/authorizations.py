fb988bfc58dfc876dffe361a447242e9d0f9a75c
Xavier Ordoquy <xordoquy@linovia.com>
2012-06-01 21:44:41 +0200

    Test names describes how the authorization works.

--- local_tests/tests/authorizations.py
+++ local_tests/tests/authorizations.py
@@ -3,6 +3,7 @@ Tests the Mixins permissions.
 """
 
 from django.core.exceptions import ImproperlyConfigured
+from  django.core.exceptions import PermissionDenied
 
 from django.test import RequestFactory
 from django.test import TestCase
@@ -19,7 +20,7 @@ class MyMixin2(Mixin):
     pass
 
 
-class ForbiddenMixin(Mixin):
+class PermissionMixin(Mixin):
 
     RESPONSE_CODE = False
 
@@ -27,26 +28,58 @@ class ForbiddenMixin(Mixin):
         return self.RESPONSE_CODE
 
 
-class ForbiddenView(View):
+class PermissionView(View):
     mixin1 = MyMixin1()
-    forbidden = ForbiddenMixin()
+    permission = PermissionMixin()
     mixin2 = MyMixin2()
+
+
+class PermissionView2(View):
+    permission1 = PermissionMixin()
+    permission2 = PermissionMixin()
 
 
 class TestAuthorization(TestCase):
 
-    def test_authorizations(self):
-        from  django.core.exceptions import PermissionDenied
-        view = ForbiddenView(mode='detail')
+    def test_denied_if_a_mixin_denies_it(self):
+        view = PermissionView(mode='detail')
+        self.assertEqual(view.default_security, 'allow')
         rf = RequestFactory()
         request = rf.get('/')
+
+        view.mixins['permission'].RESPONSE_CODE = False
         with self.assertRaises(PermissionDenied):
             view.dispatch(request)
 
-        view.mixins['forbidden'].RESPONSE_CODE = True
+    def test_authorized_if_a_mixin_authorize_it(self):
+        view = PermissionView(mode='detail')
+        self.assertEqual(view.default_security, 'allow')
+        rf = RequestFactory()
+        request = rf.get('/')
+
+        # we get ImproperlyConfigured because we have no template name
+        view.mixins['permission'].RESPONSE_CODE = True
         with self.assertRaises(ImproperlyConfigured):
             view.dispatch(request)
 
-        view.mixins['forbidden'].RESPONSE_CODE = None
+    def test_authorized_if_no_mixin_respond_and_policy_is_allow(self):
+        view = PermissionView(mode='detail')
+        view.default_security = 'allow'
+        rf = RequestFactory()
+        request = rf.get('/')
+
+        # we get ImproperlyConfigured because we have no template name
+        view.mixins['permission'].RESPONSE_CODE = None
         with self.assertRaises(ImproperlyConfigured):
+            view.dispatch(request)
+
+    def test_authorized_if_no_mixin_respond_and_policy_is_deny(self):
+        view = PermissionView(mode='detail')
+        view.default_security = 'deny'
+        rf = RequestFactory()
+        request = rf.get('/')
+
+        # we get ImproperlyConfigured because we have no template name
+        view.mixins['permission'].RESPONSE_CODE = None
+        with self.assertRaises(PermissionDenied):
             view.dispatch(request)
